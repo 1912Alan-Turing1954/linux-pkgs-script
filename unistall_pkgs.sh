cf671ff40d9a57161023ea140de88da319853e8d
Alan M. Turing <1912AlanTuring1954@gmail.com>
2024-03-01 21:44:51 -0800

    uninstall file

--- unistall_pkgs.sh
+++ unistall_pkgs.sh
@@ -4,6 +4,6 @@ declare -a PKGS=(gimp nmap vscode htop neovim vim virtualbox)
 for i in "${PKGS[@]}"; do
     check=$(command -v "$i")
     if [[ $? -ne 0 ]]; then
-        sudo apt install "$i" -y
+        sudo apt remove "$i" -y
     fi
 done
