31bf676d1072df26627e4adebb752ccf62c681c2
Alan M. Turing <1912AlanTuring1954@gmail.com>
2024-03-01 21:38:20 -0800

    -y attribute

--- install_pkgs.sh
+++ install_pkgs.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-declare -a PKGS=(brave gimp nmap vscode htop neovim vim virtualbox )
+declare -a PKGS=(brave gimp nmap vscode htop neovim vim virtualbox)
 for i in "${PKGS[@]}"; do
     check=$(command -v "$i")
     if [[ $? -ne 0 ]]; then
-        sudo apt install "$i"
+        sudo apt install "$i" -y
     fi
 done
