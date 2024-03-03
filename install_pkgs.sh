62fa980a2d443d7fe6984c33fdc6901590dd1b27
Alan M. Turing <1912AlanTuring1954@gmail.com>
2024-03-02 17:05:21 -0800

    final touches

--- install_pkgs.sh
+++ install_pkgs.sh
@@ -14,14 +14,14 @@ sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://b
 
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 
-sudo apt update
-
 sudo apt install brave-browser -y
 
 sudo apt install software-properties-common apt-transport-https wget -y
 
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 
-sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" -y
 
 sudo apt install code -y
+
+sudo apt update
