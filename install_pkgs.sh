75a68c67a6465d86d47b490adf49ace882e1d6d3
Alan M. Turing <1912AlanTuring1954@gmail.com>
2024-03-03 10:48:52 -0800

    brave browser install

--- install_pkgs.sh
+++ install_pkgs.sh
@@ -8,6 +8,32 @@ for i in "${PKGS[@]}"; do
     fi
 done
 
+echo "Intsalling brave browser, would you like to continue."
+
+confirm() {
+    read -p "(Press enter to continue or [Y/N]): " prompt
+    if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes"  || $prompt == "" || $prompt == " " $prompt == "n" || $prompt == "N" || $prompt == "no" || $prompt == "No"]]
+    then
+        sudo apt install curl
+
+        sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+
+        echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+        sudo apt install brave-browser -y
+
+        sudo apt install software-properties-common apt-transport-https wget -y
+
+        wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+
+        sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" -y
+
+        sudo apt install code -y
+    else
+        return 
+    fi
+}
+
 sudo apt install curl
 
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
--- test_brave_inst.sh
+++ test_brave_inst.sh
@@ -1,11 +1,25 @@
 #!/usr/bin/env bash
 
-sudo apt install curl -y
+confirm() {
+    read -p "(Press enter to continue or [Y/N]): " prompt
+    if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes"  || $prompt == "" || $prompt == " " $prompt == "n" || $prompt == "N" || $prompt == "no" || $prompt == "No"]]
+    then
+        sudo apt install curl
 
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+        sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+        echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 
-sudo apt update -y
+        sudo apt install brave-browser -y
 
-sudo apt install brave-browser -y
+        sudo apt install software-properties-common apt-transport-https wget -y
+
+        wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+
+        sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" -y
+
+        sudo apt install code -y
+    else
+        return 
+    fi
+}
